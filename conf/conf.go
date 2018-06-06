af6883b4b32aa1c3607302124b5acc1e18971b4d
nkostoulas <nkostoulas@gmail.com>
2018-06-06 18:01:19 +0100

    Hardcode conf in code for now, until filesystem structure is setup

--- conf/conf.go
+++ conf/conf.go
@@ -6,25 +6,43 @@
 package conf
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
-	"os"
-	"path/filepath"
+	//"os"
+	//"path/filepath"
 )
+
+var confjson = []byte(`
+{
+	"main": {
+		"rpcurl": "localhost:8333",
+		"rpcuser": "bitcoinrpc",
+		"rpcpass": "bitcoinrpcpass"
+	},
+	"ocean": {
+		"rpcurl": "localhost:18886",
+		"rpcuser": "bitcoinrpc",
+		"rpcpass": "acc1e7a299bc49449912e235b54dbce5"
+	}
+}
+`)
 
 type ClientCfg map[string]interface{}
 
 // Get config for a specific client from conf file
 func getCfg(name string) ClientCfg {
+	/* // need specific file path
 	dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
 	file, err := os.Open(dir + "/conf.json")
 	if err != nil {
 		log.Fatal(err)
 	}
-
+	*/
+	file := bytes.NewReader(confjson)
 	dec := json.NewDecoder(file)
 	var j map[string]map[string]interface{}
-	err = dec.Decode(&j)
+	err := dec.Decode(&j)
 	if err != nil {
 		log.Fatal(err)
 	}
