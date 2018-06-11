ae7d868f82f49aa797955f391184adfa21268048
nkostoulas <nkostoulas@gmail.com>
2018-06-11 11:12:47 +0100

    Allow custom config files

--- conf/conf.go
+++ conf/conf.go
@@ -1,5 +1,5 @@
 //
-// Handle reading conf files and parsing 
+// Handle reading conf files and parsing
 // configuration options
 //
 
@@ -9,37 +9,13 @@ import (
     "bytes"
     "encoding/json"
     "log"
-    //"os"
-    //"path/filepath"
 )
-
-var confjson = []byte(`
-{
-    "main": {
-        "rpcurl": "localhost:8333",
-        "rpcuser": "bitcoinrpc",
-        "rpcpass": "bitcoinrpcpass"
-    },
-    "ocean": {
-        "rpcurl": "localhost:18886",
-        "rpcuser": "bitcoinrpc",
-        "rpcpass": "acc1e7a299bc49449912e235b54dbce5"
-    }
-}
-`)
 
 type ClientCfg map[string]interface{}
 
 // Get config for a specific client from conf file
-func getCfg(name string) ClientCfg {
-    /* // need specific file path
-    dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
-    file, err := os.Open(dir + "/conf.json")
-    if err != nil {
-        log.Fatal(err)
-    }
-    */
-    file := bytes.NewReader(confjson)
+func getCfg(name string, conf []byte) ClientCfg {
+    file := bytes.NewReader(conf)
     dec := json.NewDecoder(file)
     var j map[string]map[string]interface{}
     err := dec.Decode(&j)
--- conf/rpc.go
+++ conf/rpc.go
@@ -9,9 +9,28 @@ import (
     "github.com/btcsuite/btcd/rpcclient"
 )
 
-// Get RPC connection for a client from the conf file
-func GetRPC(name string) *rpcclient.Client{
-    cfg := getCfg(name)
+var myConf = []byte(`
+{
+    "main": {
+        "rpcurl": "localhost:8333",
+        "rpcuser": "bitcoinrpc",
+        "rpcpass": "bitcoinrpcpass"
+    },
+    "ocean": {
+        "rpcurl": "localhost:18886",
+        "rpcuser": "bitcoinrpc",
+        "rpcpass": "acc1e7a299bc49449912e235b54dbce5"
+    }
+}
+`)
+
+// Get RPC connection for a client from a conf file
+func GetRPC(name string, customConf ...[]byte) *rpcclient.Client{
+    conf := myConf
+    if len(customConf) > 0 { //custom config provided
+        conf = customConf[0]
+    }
+    cfg := getCfg(name, conf)
     connCfg := &rpcclient.ConnConfig{
         Host:         cfg.getValue("rpcurl"),
         User:         cfg.getValue("rpcuser"),
