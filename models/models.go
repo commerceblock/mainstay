0d7567840da9319f1d8be3d05a1be632fecf37b7
lodenos <nikolaiilodenos@gmail.com>
2018-10-22 12:40:36 +0100

    move Response/Request/Channel to separate files #7

--- models/models.go
+++ models/models.go
@@ -1,44 +1,27 @@
 // Package models provides structs for request/reponse service communications.
 package models
 
-type Request struct {
-    Name      string    `json:"name"`
-    Id        string    `json:"id"`
-}
-
-type Response struct {
-    Request   Request
-    Error     string    `json:"error"`
-}
-type BlockResponse struct {
-    Response
-    Attested    bool    `json:"attested"`
-}
-type BestBlockResponse struct {
-    Response
-    BlockHash   string    `json:"blockhash"`
-}
 type BestBlockHeightResponse struct {
-    Response
-    BlockHeight int32    `json:"blockheight"`
+	Response
+	BlockHeight int32 `json:"blockheight"`
 }
-type TransactionResponse struct {
-    Response
-    Attested    bool    `json:"attested"`
+
+type BestBlockResponse struct {
+	Response
+	BlockHash string `json:"blockhash"`
 }
+
+type BlockResponse struct {
+	Response
+	Attested bool `json:"attested"`
+}
+
 type LatestAttestationResponse struct {
-    Response
-    TxHash      string    `json:"txhash"`
+	Response
+	TxHash string `json:"txhash"`
 }
 
-type Channel struct {
-    Requests    chan Request
-    Responses   chan interface{}
-}
-
-func NewChannel() *Channel {
-    channel := &Channel{}
-    channel.Requests = make(chan Request)
-    channel.Responses = make(chan interface{})
-    return channel
+type TransactionResponse struct {
+	Response
+	Attested bool `json:"attested"`
 }
