7c51bec541a05f284129a58630b12a2d79704457
nkostoulas <nkostoulas@gmail.com>
2018-10-29 11:20:21 +0000

    Crypto file split and rename

---
+++ crypto/scripts.go
@@ -0,0 +1,115 @@
+// Package crypto contains utilities for generating and validation attestation addresses.
+package crypto
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"strconv"
+
+	"github.com/btcsuite/btcd/btcec"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil"
+)
+
+// Raw method to parse a multisig script and get pubkeys and num of sigs
+func ParseRedeemScript(script string) ([]*btcec.PublicKey, int) {
+
+	// check op codes
+	lscript := len(script)
+	op := script[0]
+	op1 := script[lscript-4]
+	if !(string(op) == string(op1)) && (string(op1) == "5") {
+		log.Fatal("Incorrect opcode in redeem script")
+	}
+
+	// check multisig
+	if script[lscript-2:] != "ae" {
+		log.Fatal("Checkmultisig missing from redeem script")
+	}
+
+	numOfSigs, _ := strconv.Atoi(string(script[1]))
+	numOfKeys, _ := strconv.Atoi(string(script[lscript-3]))
+
+	var startIndex int64 = 2
+	var keys []*btcec.PublicKey
+	for i := 0; i < numOfKeys; i++ {
+		keysize, _ := strconv.ParseInt(string(script[startIndex:startIndex+2]), 16, 16)
+		if !(keysize == 65 || keysize == 33) {
+			log.Fatal("Incorrect pubkey size")
+		}
+		keystr := script[startIndex+2 : startIndex+2+2*keysize]
+		keybytes, _ := hex.DecodeString(keystr)
+		pubkey, err := btcec.ParsePubKey(keybytes, btcec.S256())
+		if err != nil {
+			log.Fatal(err)
+		}
+		startIndex += 2 + 2*keysize
+		keys = append(keys, pubkey)
+	}
+	return keys, numOfSigs
+}
+
+// Raw method to create a multisig from pubkeys and return P2SH address and redeemScript
+func CreateMultisig(pubkeys []*btcec.PublicKey, nSigs int, chainCfg *chaincfg.Params) (btcutil.Address, string) {
+
+	var script string
+	script += fmt.Sprintf("5%d", nSigs)
+
+	for _, pub := range pubkeys {
+		script += "21"
+		script += hex.EncodeToString(pub.SerializeCompressed())
+	}
+
+	script += fmt.Sprintf("5%d", len(pubkeys))
+	script += "ae"
+
+	scriptBytes, _ := hex.DecodeString(script)
+	multisigAddr, err := btcutil.NewAddressScriptHash(scriptBytes, chainCfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return multisigAddr, script
+}
+
+// Parse scriptSig and return sigs and redeemScript
+func ParseScriptSig(scriptSig []byte) ([][]byte, []byte) {
+
+	if len(scriptSig) == 0 {
+		return [][]byte{}, []byte{}
+	}
+
+	var scripts [][]byte
+	it := 1
+	for {
+		scriptSize := scriptSig[it]
+		script := scriptSig[it+1 : it+1+int(scriptSize)]
+		scripts = append(scripts, script)
+
+		it += 1 + int(scriptSize)
+
+		if len(scriptSig) <= it {
+			break
+		}
+	}
+
+	return scripts[:len(scripts)-1], scripts[len(scripts)-1]
+}
+
+// Create scriptSig from sigs and redeemScript
+func CreateScriptSig(sigs [][]byte, script []byte) []byte {
+
+	var scriptSig []byte
+	scriptSig = append(scriptSig, byte(0))
+
+	for _, sig := range sigs {
+		scriptSig = append(scriptSig, byte(len(sig)))
+		scriptSig = append(scriptSig, sig...)
+	}
+
+	scriptSig = append(scriptSig, byte(len(script)))
+	scriptSig = append(scriptSig, script...)
+
+	return scriptSig
+}
--- crypto/scripts_test.go
+++ crypto/scripts_test.go
@@ -2,7 +2,6 @@ package crypto
 
 import (
 	"encoding/hex"
-	"mainstay/clients"
 	"mainstay/config"
 	"mainstay/test"
 	"testing"
@@ -21,45 +20,6 @@ func init() {
 	test := test.NewTest(false, false)
 	testConfig = test.Config
 	mainChainCfg = testConfig.MainChainCfg()
-}
-
-// Test Tweaking utility
-func TestTweaking(t *testing.T) {
-	sideClientFake := testConfig.OceanClient().(*clients.SidechainClientFake)
-
-	tweak, _ := sideClientFake.GetBestBlockHash()
-
-	// test GetWalletPrivKey
-	privKey := GetWalletPrivKey(testConfig.InitPK())
-	assert.Equal(t, "cQca2KvrBnJJUCYa2tD4RXhiQshWLNMSK2A96ZKWo1SZkHhh3YLz", privKey.String())
-
-	// test TweakprivKey
-	tweakedPrivKey := TweakPrivKey(privKey, tweak.CloneBytes(), mainChainCfg)
-	assert.Equal(t, "cNBumPP9tv3CZgBMnC6srrFMqkmCp5AhaYhWGLTQhs4zRKJCKRe4", tweakedPrivKey.String())
-
-	// test GetAddressFromPrivKey and IsAddrTweakedFromHash
-	addr := GetAddressFromPrivKey(tweakedPrivKey, mainChainCfg)
-	assert.Equal(t, true, IsAddrTweakedFromHash(addr.String(), tweak.CloneBytes(), privKey, mainChainCfg))
-	assert.Equal(t, "mgYhSzKCdWzV6c7mBn9EzXkEADVBmPJHmi", addr.String())
-
-	// Test TweakPubKey and GetAddressFromPubKey
-	pubkey := privKey.PrivKey.PubKey()
-	tweakedPubKey := TweakPubKey(pubkey, tweak.CloneBytes())
-	tweakedPubKeyAddr := GetAddressFromPubKey(tweakedPubKey, mainChainCfg)
-	assert.Equal(t, addr.String(), tweakedPubKeyAddr.String())
-
-	// Validate tweaked private key
-	importErr := testConfig.MainClient().ImportPrivKey(tweakedPrivKey)
-	assert.Equal(t, nil, importErr)
-
-	// Validate address generated from tweaked key
-	tx, newTxErr := testConfig.MainClient().SendToAddress(addr, 10000)
-	assert.Equal(t, nil, newTxErr)
-
-	// Check transaction is in the wallet after importing key
-	txres, txResErr := testConfig.MainClient().GetTransaction(tx)
-	assert.Equal(t, nil, txResErr)
-	assert.Equal(t, tx.String(), txres.TxID)
 }
 
 // Test Multisig utility
