7be5575e820726dd57aabbbd66e69a200ab7db1e
nkostoulas <nkostoulas@gmail.com>
2018-11-06 11:58:42 +0000

    Remove ocean from main service

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"sync"
 
 	"mainstay/attestation"
-	"mainstay/clients"
 	"mainstay/config"
 	"mainstay/server"
 	"mainstay/test"
@@ -23,7 +22,6 @@ var (
 	isRegtest  bool
 	apiHost    string
 	mainConfig *config.Config
-	ocean      clients.SidechainClient
 )
 
 func parseFlags() {
@@ -45,25 +43,22 @@ func init() {
 	if isRegtest {
 		test := test.NewTest(true, true)
 		mainConfig = test.Config
-		ocean = test.OceanClient
 		log.Printf("Running regtest mode with -tx=%s\n", mainConfig.InitTX())
 	} else {
 		mainConfig = config.NewConfig()
 		mainConfig.SetInitTX(tx0)
 		mainConfig.SetInitPK(pk0)
 		mainConfig.SetMultisigScript(script)
-		ocean = config.NewClientFromConfig(false)
 	}
 }
 
 func main() {
 	defer mainConfig.MainClient().Shutdown()
-	defer ocean.Close()
 
 	wg := &sync.WaitGroup{}
 	ctx, cancel := context.WithCancel(context.Background())
 
-	server := server.NewServer(ctx, mainConfig, ocean)
+	server := server.NewServer(ctx, mainConfig)
 	attestService := attestation.NewAttestService(ctx, wg, server, mainConfig)
 
 	c := make(chan os.Signal)
--- server/server.go
+++ server/server.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log"
 
-	"mainstay/clients"
 	"mainstay/config"
 	"mainstay/models"
 
@@ -15,33 +14,24 @@ import (
 // Stores information on the latest attestation and commitment
 // Methods to get latest state by attestation service
 type Server struct {
-	latestAttestation models.Attestation
-	latestCommitment  *models.Commitment
-
-	// to remove soon
-	sideClient clients.SidechainClient
+	dbInterface DbMongo
 }
 
-var dbInterface DbMongo
-
 // NewServer returns a pointer to an Server instance
-func NewServer(ctx context.Context, config *config.Config, sideClient clients.SidechainClient) *Server {
+func NewServer(ctx context.Context, config *config.Config) *Server {
 
-	var dbErr error
-	dbInterface, dbErr = NewDbMongo(ctx)
+	dbInterface, dbErr := NewDbMongo(ctx)
 	if dbErr != nil {
 		log.Fatal(dbErr)
 	}
 
-	return &Server{*models.NewAttestationDefault(), (*models.Commitment)(nil), sideClient}
+	return &Server{dbInterface}
 }
 
 // Update latest attestation in the server
 func (s *Server) UpdateLatestAttestation(attestation models.Attestation) error {
 
-	s.latestAttestation = attestation // to remove
-
-	errSave := dbInterface.saveAttestation(attestation)
+	errSave := s.dbInterface.saveAttestation(attestation)
 	if errSave != nil {
 		return errSave
 	}
@@ -49,7 +39,7 @@ func (s *Server) UpdateLatestAttestation(attestation models.Attestation) error {
 	if errCommitment != nil {
 		return errCommitment
 	}
-	errSave = dbInterface.saveCommitment(*commitment)
+	errSave = s.dbInterface.saveCommitment(*commitment)
 	if errSave != nil {
 		return errSave
 	}
@@ -60,30 +50,21 @@ func (s *Server) UpdateLatestAttestation(attestation models.Attestation) error {
 func (s *Server) GetLatestAttestedCommitmentHash() (chainhash.Hash, error) {
 
 	//db interface
-	return dbInterface.getLatestAttestedCommitmentHash()
+	return s.dbInterface.getLatestAttestedCommitmentHash()
 }
 
 // Return latest commitment stored in the server
 func (s *Server) GetLatestCommitment() (models.Commitment, error) {
 
 	// db interface
-	return dbInterface.getLatestCommitment()
+	return s.dbInterface.getLatestCommitment()
 }
 
 // Return commitment for a particular attestation transaction id
 func (s *Server) GetAttestationCommitment(attestationTxid chainhash.Hash) (models.Commitment, error) {
 
 	// db interface
-	_, _ = dbInterface.getAttestationCommitment(attestationTxid)
+	_, _ = s.dbInterface.getAttestationCommitment(attestationTxid)
 
 	return models.Commitment{}, nil
-}
-
-// TODO REMOVE: Update latest commitment hash
-func (s *Server) updateCommitment() {
-	hash, err := s.sideClient.GetBestBlockHash()
-	if err != nil {
-		log.Fatal(err)
-	}
-	s.latestCommitment, _ = models.NewCommitment([]chainhash.Hash{*hash, *hash, *hash})
 }
