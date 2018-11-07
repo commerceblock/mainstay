db6d6f53b221fb1d10e20a55b51c66990326b025
nkostoulas <nkostoulas@gmail.com>
2018-11-07 11:08:28 +0000

    Error when intermediary client position missing

--- server/server.go
+++ server/server.go
@@ -1,6 +1,9 @@
 package server
 
 import (
+	"errors"
+	"fmt"
+
 	"mainstay/models"
 
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
@@ -37,7 +40,7 @@ func (s *Server) updateAttestationCommitment(commitment models.Commitment) error
 	return nil
 }
 
-// Update latest attestation in the server
+// Update latest Attestation in the server
 func (s *Server) UpdateLatestAttestation(attestation models.Attestation) error {
 
 	errSave := s.dbInterface.saveAttestation(attestation)
@@ -55,10 +58,10 @@ func (s *Server) UpdateLatestAttestation(attestation models.Attestation) error {
 	return nil
 }
 
-// Return latest attestation stored in the server
+// Return Commitment hash of latest Attestation stored in the server
 func (s *Server) GetLatestAttestationCommitmentHash() (chainhash.Hash, error) {
 
-	//db interface
+	// get attestation merkle root from db
 	merkleRoot, rootErr := s.dbInterface.getLatestAttestationMerkleRoot()
 	if rootErr != nil {
 		return chainhash.Hash{}, rootErr
@@ -72,39 +75,18 @@ func (s *Server) GetLatestAttestationCommitmentHash() (chainhash.Hash, error) {
 	return *commitmentHash, nil
 }
 
-// Return latest commitment stored in the server
-func (s *Server) GetLatestCommitment() (models.Commitment, error) {
-
-	latestCommitments, errLatest := s.dbInterface.getLatestCommitments()
-	if errLatest != nil {
-		return models.Commitment{}, errLatest
-	}
-
-	var commitmentHashes []chainhash.Hash
-	for _, c := range latestCommitments {
-		commitmentHashes = append(commitmentHashes, c.Commitment)
-	}
-	// construct Commitment from MerkleCommitment commitments
-	commitment, errCommitment := models.NewCommitment(commitmentHashes)
-	if errCommitment != nil {
-		return models.Commitment{}, nil
-	}
-
-	// db interface
-	return *commitment, nil
-}
-
-// Return commitment for a particular attestation transaction id
+// Return Commitment for a particular Attestation transaction id
 func (s *Server) GetAttestationCommitment(attestationTxid chainhash.Hash) (models.Commitment, error) {
 
-	// db interface
+	// get merkle commitments from db
 	merkleCommitments, _ := s.dbInterface.getAttestationMerkleCommitments(attestationTxid)
 
+	// construct Commitment from MerkleCommitment commitments
 	var commitmentHashes []chainhash.Hash
 	for _, c := range merkleCommitments {
 		commitmentHashes = append(commitmentHashes, c.Commitment)
 	}
-	// construct Commitment from MerkleCommitment commitments
+
 	_, errCommitment := models.NewCommitment(commitmentHashes)
 	if errCommitment != nil {
 		return models.Commitment{}, nil
@@ -114,4 +96,32 @@ func (s *Server) GetAttestationCommitment(attestationTxid chainhash.Hash) (model
 	// if not found return error
 
 	return models.Commitment{}, nil
+}
+
+// Return latest commitment stored in the server
+func (s *Server) GetLatestCommitment() (models.Commitment, error) {
+
+	// get latest commitments from db
+	latestCommitments, errLatest := s.dbInterface.getLatestCommitments()
+	if errLatest != nil {
+		return models.Commitment{}, errLatest
+	}
+
+	var commitmentHashes []chainhash.Hash
+	// assume latest commitments ordered by position
+	for pos, c := range latestCommitments {
+		if int32(pos) == c.ClientPosition {
+			commitmentHashes = append(commitmentHashes, c.Commitment)
+		} else {
+			return models.Commitment{}, errors.New(fmt.Sprintf("Latest commitment missing in position %d\n", pos))
+		}
+	}
+	// construct Commitment from MerkleCommitment commitments
+	commitment, errCommitment := models.NewCommitment(commitmentHashes)
+	if errCommitment != nil {
+		return models.Commitment{}, nil
+	}
+
+	// db interface
+	return *commitment, nil
 }
