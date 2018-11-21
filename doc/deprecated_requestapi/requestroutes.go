153a4f63d9d5c61da7a801af61286b84f9bb8bf2
nkostoulas <nkostoulas@gmail.com>
2018-11-21 16:08:21 +0000

    Comment out deprecated requestapi

--- doc/deprecated_requestapi/requestroutes.go
+++ doc/deprecated_requestapi/requestroutes.go
@@ -1,92 +1,92 @@
 package requestapi
 
-import (
-	"log"
-	"net/http"
-	"time"
+// import (
+// 	"log"
+// 	"net/http"
+// 	"time"
 
-	"github.com/gorilla/mux"
-)
+// 	"github.com/gorilla/mux"
+// )
 
-const GET = "GET"
-const POST = "POST"
+// const GET = "GET"
+// const POST = "POST"
 
-const (
-	ROUTE_INDEX              = "/"
-	ROUTE_VERIFY_BLOCK       = "/api/verifyblock/{blockId}"
-	ROUTE_BEST_BLOCK         = "/api/bestblock/"
-	ROUTE_BEST_BLOCK_HEIGHT  = "/api/bestblockheight/"
-	ROUTE_COMMITMENT_SEND    = "/api/commitment/send/"
-	ROUTE_LATEST_ATTESTATION = "/api/latestattestation/"
-)
+// const (
+// 	ROUTE_INDEX              = "/"
+// 	ROUTE_VERIFY_BLOCK       = "/api/verifyblock/{blockId}"
+// 	ROUTE_BEST_BLOCK         = "/api/bestblock/"
+// 	ROUTE_BEST_BLOCK_HEIGHT  = "/api/bestblockheight/"
+// 	ROUTE_COMMITMENT_SEND    = "/api/commitment/send/"
+// 	ROUTE_LATEST_ATTESTATION = "/api/latestattestation/"
+// )
 
-// Route structure
-// Routing for http requests to request service
-type Route struct {
-	name        string
-	method      string
-	pattern     string
-	handlerFunc func(http.ResponseWriter, *http.Request, *Channel)
-}
+// // Route structure
+// // Routing for http requests to request service
+// type Route struct {
+// 	name        string
+// 	method      string
+// 	pattern     string
+// 	handlerFunc func(http.ResponseWriter, *http.Request, *Channel)
+// }
 
-var routes = []Route{
-	Route{
-		SERVER_INDEX,
-		GET,
-		ROUTE_INDEX,
-		HandleIndex,
-	},
-	Route{
-		SERVER_BEST_BLOCK,
-		GET,
-		ROUTE_BEST_BLOCK,
-		HandleBestBlock,
-	},
-	Route{
-		SERVER_BEST_BLOCK_HEIGHT,
-		GET,
-		ROUTE_BEST_BLOCK_HEIGHT,
-		HandleBestBlockHeight,
-	},
-	Route{
-		SERVER_VERIFY_BLOCK,
-		GET,
-		ROUTE_VERIFY_BLOCK,
-		HandleVerifyBlock,
-	},
-	Route{
-		SERVER_LATEST_ATTESTATION,
-		GET,
-		ROUTE_LATEST_ATTESTATION,
-		HandleLatestAttestation,
-	},
-	Route{
-		SERVER_COMMITMENT_SEND,
-		POST,
-		ROUTE_COMMITMENT_SEND,
-		HandleCommitmentSend,
-	},
-}
+// var routes = []Route{
+// 	Route{
+// 		SERVER_INDEX,
+// 		GET,
+// 		ROUTE_INDEX,
+// 		HandleIndex,
+// 	},
+// 	Route{
+// 		SERVER_BEST_BLOCK,
+// 		GET,
+// 		ROUTE_BEST_BLOCK,
+// 		HandleBestBlock,
+// 	},
+// 	Route{
+// 		SERVER_BEST_BLOCK_HEIGHT,
+// 		GET,
+// 		ROUTE_BEST_BLOCK_HEIGHT,
+// 		HandleBestBlockHeight,
+// 	},
+// 	Route{
+// 		SERVER_VERIFY_BLOCK,
+// 		GET,
+// 		ROUTE_VERIFY_BLOCK,
+// 		HandleVerifyBlock,
+// 	},
+// 	Route{
+// 		SERVER_LATEST_ATTESTATION,
+// 		GET,
+// 		ROUTE_LATEST_ATTESTATION,
+// 		HandleLatestAttestation,
+// 	},
+// 	Route{
+// 		SERVER_COMMITMENT_SEND,
+// 		POST,
+// 		ROUTE_COMMITMENT_SEND,
+// 		HandleCommitmentSend,
+// 	},
+// }
 
-// NewRouter returns pointer to mux router instance
-func NewRouter(channel *Channel) *mux.Router {
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-		handlerFunc := makeHandler(route.handlerFunc, channel) // pass channel to request handler
-		router.
-			Methods(route.method).
-			Path(route.pattern).
-			Name(route.name).
-			Handler(handlerFunc)
-	}
-	return router
-}
+// // NewRouter returns pointer to mux router instance
+// func NewRouter(channel *Channel) *mux.Router {
+// 	router := mux.NewRouter().StrictSlash(true)
+// 	for _, route := range routes {
+// 		handlerFunc := makeHandler(route.handlerFunc, channel) // pass channel to request handler
+// 		router.
+// 			Methods(route.method).
+// 			Path(route.pattern).
+// 			Name(route.name).
+// 			Handler(handlerFunc)
+// 	}
+// 	return router
+// }
 
-// make custom handler to pass communication channel between server and api
-func makeHandler(fn func(http.ResponseWriter, *http.Request, *Channel), channel *Channel) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		fn(w, r, channel)
-		log.Printf("%s\t%s\t%s", r.Method, r.RequestURI, time.Since(start))
-	}
-}
+// // make custom handler to pass communication channel between server and api
+// func makeHandler(fn func(http.ResponseWriter, *http.Request, *Channel), channel *Channel) http.HandlerFunc {
+// 	return func(w http.ResponseWriter, r *http.Request) {
+// 		start := time.Now()
+// 		fn(w, r, channel)
+// 		log.Printf("%s\t%s\t%s", r.Method, r.RequestURI, time.Since(start))
+// 	}
+// }
--- doc/deprecated_requestapi/requestservice.go
+++ doc/deprecated_requestapi/requestservice.go
@@ -1,77 +1,77 @@
 // Package requestapi implements the request api service that listens to attestation requests.
 package requestapi
 
-import (
-	"context"
-	"log"
-	"net/http"
-	"sync"
-	"time"
+// import (
+// 	"context"
+// 	"log"
+// 	"net/http"
+// 	"sync"
+// 	"time"
 
-	"github.com/gorilla/mux"
-)
+// 	"github.com/gorilla/mux"
+// )
 
-// RequestService struct
-// Handles setting a request router and handling api requests
-type RequestService struct {
-	ctx           context.Context
-	wg            *sync.WaitGroup
-	host          string
-	router        *mux.Router
-	channel       *Channel
-	serverChannel chan RequestWithResponseChannel
-}
+// // RequestService struct
+// // Handles setting a request router and handling api requests
+// type RequestService struct {
+// 	ctx           context.Context
+// 	wg            *sync.WaitGroup
+// 	host          string
+// 	router        *mux.Router
+// 	channel       *Channel
+// 	serverChannel chan RequestWithResponseChannel
+// }
 
-// NewRequestService returns a pointer to a RequestService instance
-func NewRequestService(ctx context.Context, wg *sync.WaitGroup, serverChannel chan RequestWithResponseChannel, host string) *RequestService {
-	channel := NewChannel()
-	router := NewRouter(channel)
-	return &RequestService{ctx, wg, host, router, channel, serverChannel}
-}
+// // NewRequestService returns a pointer to a RequestService instance
+// func NewRequestService(ctx context.Context, wg *sync.WaitGroup, serverChannel chan RequestWithResponseChannel, host string) *RequestService {
+// 	channel := NewChannel()
+// 	router := NewRouter(channel)
+// 	return &RequestService{ctx, wg, host, router, channel, serverChannel}
+// }
 
-// Main Run method
-func (c *RequestService) Run() {
-	defer c.wg.Done()
+// // Main Run method
+// func (c *RequestService) Run() {
+// 	defer c.wg.Done()
 
-	srv := &http.Server{
-		Addr:         c.host,
-		WriteTimeout: time.Second * 15,
-		ReadTimeout:  time.Second * 15,
-		Handler:      c.router,
-	}
+// 	srv := &http.Server{
+// 		Addr:         c.host,
+// 		WriteTimeout: time.Second * 15,
+// 		ReadTimeout:  time.Second * 15,
+// 		Handler:      c.router,
+// 	}
 
-	c.wg.Add(1)
-	go func() { //Running server waiting for requests
-		defer c.wg.Done()
-		if err := srv.ListenAndServe(); err != nil {
-			log.Println(err)
-		}
-	}()
+// 	c.wg.Add(1)
+// 	go func() { //Running server waiting for requests
+// 		defer c.wg.Done()
+// 		if err := srv.ListenAndServe(); err != nil {
+// 			log.Println(err)
+// 		}
+// 	}()
 
-	c.wg.Add(1)
-	go func() { //Waiting for requests from the request service and pass to server for response
-		defer c.wg.Done()
-		for {
-			select {
-			case <-c.ctx.Done():
-				return
-				// receive requests from http server and pass on to main attestation Server
-				// provide a response channel along with the request, in order to pick up
-				// the response and serve it to the clients that sent the request
-			case req := <-c.channel.Requests:
-				c.serverChannel <- RequestWithResponseChannel{req, c.channel.Responses}
-			}
-		}
-	}()
+// 	c.wg.Add(1)
+// 	go func() { //Waiting for requests from the request service and pass to server for response
+// 		defer c.wg.Done()
+// 		for {
+// 			select {
+// 			case <-c.ctx.Done():
+// 				return
+// 				// receive requests from http server and pass on to main attestation Server
+// 				// provide a response channel along with the request, in order to pick up
+// 				// the response and serve it to the clients that sent the request
+// 			case req := <-c.channel.Requests:
+// 				c.serverChannel <- RequestWithResponseChannel{req, c.channel.Responses}
+// 			}
+// 		}
+// 	}()
 
-	c.wg.Add(1)
-	go func() { //Waiting for cancellation signal to shut down server
-		defer c.wg.Done()
-		select {
-		case <-c.ctx.Done():
-			log.Println("Shutting down request service...")
-			srv.Shutdown(c.ctx)
-			return
-		}
-	}()
-}
+// 	c.wg.Add(1)
+// 	go func() { //Waiting for cancellation signal to shut down server
+// 		defer c.wg.Done()
+// 		select {
+// 		case <-c.ctx.Done():
+// 			log.Println("Shutting down request service...")
+// 			srv.Shutdown(c.ctx)
+// 			return
+// 		}
+// 	}()
+// }
