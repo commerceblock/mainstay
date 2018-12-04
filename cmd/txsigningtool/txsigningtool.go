e49c12abfdfb4bb6464fb538326607203d8d8b96
nkostoulas <nkostoulas@gmail.com>
2018-12-04 13:44:15 +0000

    Add topup process to txsigningtool

--- cmd/txsigningtool/txsigningtool.go
+++ cmd/txsigningtool/txsigningtool.go
@@ -31,16 +31,27 @@ import (
 // with the main attestation service to receive latest commitments and sign transactions
 
 var (
-	tx0          string
-	pk0          string
-	script       string
-	isRegtest    bool
-	sub          *messengers.SubscriberZmq
-	pub          *messengers.PublisherZmq
-	attestedHash chainhash.Hash
-	nextHash     chainhash.Hash
-	poller       *zmq.Poller
-	client       *attestation.AttestClient
+	// use attest client interface for signing
+	client    *attestation.AttestClient
+	isRegtest bool
+
+	// init transaction parameters
+	pk0     string
+	script0 string
+
+	// topup parameters
+	addrTopup   string
+	pkTopup     string
+	scriptTopup string
+
+	// communication with attest service
+	sub    *messengers.SubscriberZmq
+	pub    *messengers.PublisherZmq
+	poller *zmq.Poller
+	host   string
+
+	attestedHash chainhash.Hash // previous attested hash
+	nextHash     chainhash.Hash // next hash to sign with
 )
 
 // main conf path for main use in attestation
@@ -52,9 +63,13 @@ const DEMO_INIT_PATH = "/src/mainstay/cmd/txsigningtool/demo-init-signingtool.sh
 
 func parseFlags() {
 	flag.BoolVar(&isRegtest, "regtest", false, "Use regtest wallet configuration")
-	flag.StringVar(&tx0, "tx", "", "Tx id for genesis attestation transaction")
 	flag.StringVar(&pk0, "pk", "", "Client pk for genesis attestation transaction")
-	flag.StringVar(&script, "script", "", "Redeem script in case multisig is used")
+	flag.StringVar(&script0, "script", "", "Redeem script in case multisig is used")
+	flag.StringVar(&addrTopup, "addrTopup", "", "Address for topup transaction")
+	flag.StringVar(&pk0, "pkTopup", "", "Client pk for topup address")
+	flag.StringVar(&scriptTopup, "scriptTopup", "", "Redeem script for topup")
+
+	flag.StringVar(&host, "host", "*:5001", "Client host to publish signatures at")
 	flag.Parse()
 
 	if pk0 == "" && !isRegtest {
@@ -67,8 +82,10 @@ func init() {
 	parseFlags()
 	var config *confpkg.Config
 
+	// regtest mode
+	// run demo init script to setup bitcoin node and initial transaction
+	// generate test config using demo config file
 	if isRegtest {
-		// btc regtest node setup
 		cmd := exec.Command("/bin/sh", os.Getenv("GOPATH")+DEMO_INIT_PATH)
 		_, err := cmd.Output()
 		if err != nil {
@@ -85,8 +102,13 @@ func init() {
 			log.Fatal(configErr)
 		}
 		pk0 = test.PRIV_CLIENT
-		script = test.SCRIPT
+		script0 = test.SCRIPT
+		pkTopup = test.PRIV_CLIENT
+		scriptTopup = test.SCRIPT
+		addrTopup = test.ADDRESS
 	} else {
+		// regular mode
+		// use conf file to setup config
 		confFile, confErr := confpkg.GetConfFile(os.Getenv("GOPATH") + CONF_PATH)
 		if confErr != nil {
 			log.Fatal(confErr)
@@ -97,20 +119,33 @@ func init() {
 			log.Fatal(configErr)
 		}
 
-		// if either tx or script not set throw error
-		if tx0 == "" || script == "" {
-			if config.InitTx() == "" || config.InitScript() == "" {
-				flag.PrintDefaults()
-				log.Fatalf(`Need to provide both -tx and -script argument.
-                    To use test configuration set the -regtest flag.`)
-			}
+		// if init script is not set throw error
+		if script0 == "" && config.InitScript() == "" {
+			flag.PrintDefaults()
+			log.Fatalf(`Need to provide -script argument.
+                To use test configuration set the -regtest flag.`)
 		}
 	}
 
-	config.SetInitTx(tx0)
-	config.SetInitPK(pk0)
-	config.SetInitScript(script)
-	client = attestation.NewAttestClient(config, true) // isSigner flag set
+	// overwrite init config if set from command line
+	if pk0 != "" {
+		config.SetInitPK(pk0)
+	}
+	if script0 != "" {
+		config.SetInitScript(script0)
+	}
+
+	// overwrite topup config if set from command line
+	if pkTopup != "" {
+		config.SetTopupPK(pkTopup)
+	}
+	if addrTopup != "" && scriptTopup != "" {
+		config.SetTopupAddress(addrTopup)
+		config.SetTopupScript(scriptTopup)
+	}
+
+	// init client interface with isSigner flag set
+	client = attestation.NewAttestClient(config, true)
 
 	// get publisher addr from config, if set
 	publisherAddr := fmt.Sprintf("127.0.0.1:%d", attestation.DEFAULT_MAIN_PUBLISHER_PORT)
@@ -122,7 +157,7 @@ func init() {
 	poller = zmq.NewPoller()
 	topics := []string{attestation.TOPIC_NEW_HASH, attestation.TOPIC_NEW_TX, attestation.TOPIC_CONFIRMED_HASH}
 	sub = messengers.NewSubscriberZmq(publisherAddr, topics, poller)
-	pub = messengers.NewPublisherZmq("*:5001", poller)
+	pub = messengers.NewPublisherZmq(host, poller)
 }
 
 func main() {
@@ -197,10 +232,15 @@ func processTx(msg []byte) {
 		log.Fatal(signErr)
 	}
 
-	scriptSig := signedMsgTx.TxIn[0].SignatureScript
-	if len(scriptSig) > 0 {
-		sigs, _ := crypto.ParseScriptSig(scriptSig)
-		fmt.Printf("sending sig %s\n", hex.EncodeToString(sigs[0]))
-		pub.SendMessage(sigs[0], attestation.TOPIC_SIGS)
+	var mySigs []byte
+	for i, txin := range signedMsgTx.TxIn {
+		scriptSig := txin.SignatureScript
+		if len(scriptSig) > 0 {
+			sigs, _ := crypto.ParseScriptSig(scriptSig)
+			fmt.Printf("sending sig(%d) %s\n", i, hex.EncodeToString(sigs[0]))
+			mySigs = append(mySigs, byte(len(sigs[0])))
+			mySigs = append(mySigs, sigs[0]...)
+		}
 	}
+	pub.SendMessage(mySigs, attestation.TOPIC_SIGS)
 }
