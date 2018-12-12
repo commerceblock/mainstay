495a55c57b3194abdb9480d2843390320f504663
nkostoulas <nkostoulas@gmail.com>
2018-12-12 12:43:12 +0000

    fmt signingtool

--- cmd/txsigningtool/txsigningtool.go
+++ cmd/txsigningtool/txsigningtool.go
@@ -5,13 +5,13 @@
 package main
 
 import (
+	"encoding/hex"
 	"flag"
 	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"time"
-    "encoding/hex"
 
 	"mainstay/attestation"
 	confpkg "mainstay/config"
@@ -19,8 +19,8 @@ import (
 	"mainstay/messengers"
 	"mainstay/test"
 
+	"github.com/btcsuite/btcd/btcec"
 	"github.com/btcsuite/btcd/chaincfg/chainhash"
-    "github.com/btcsuite/btcd/btcec"
 	zmq "github.com/pebbe/zmq4"
 )
 
@@ -192,39 +192,39 @@ func processHash(msg []byte) chainhash.Hash {
 // Process received tx, verify and reply with signature
 func processTx(msg []byte) {
 
-    var sigs [][]byte
+	var sigs [][]byte
 
-    // get tx pre images from message
-    txPreImages := attestation.UnserializeBytes(msg)
+	// get tx pre images from message
+	txPreImages := attestation.UnserializeBytes(msg)
 
-    // process each pre image transaction and sign
-    for txIt, txPreImage := range txPreImages {
-        // add hash type to tx serialization
-        txPreImage = append(txPreImage, []byte{1, 0, 0, 0}...)
-        txPreImageHash := chainhash.DoubleHashH(txPreImage)
+	// process each pre image transaction and sign
+	for txIt, txPreImage := range txPreImages {
+		// add hash type to tx serialization
+		txPreImage = append(txPreImage, []byte{1, 0, 0, 0}...)
+		txPreImageHash := chainhash.DoubleHashH(txPreImage)
 
-        // sign first tx with tweaked priv key and
-        // any remaining txs with topup key
-        var sig *btcec.Signature
-        var signErr error
-        if txIt == 0 {
-            priv := client.GetKeyFromHash(attestedHash).PrivKey
-            sig, signErr = priv.Sign(txPreImageHash.CloneBytes())
-        } else {
-            sig, signErr = client.WalletPrivTopup.PrivKey.Sign(txPreImageHash.CloneBytes())
-        }
-        if signErr != nil {
-            log.Fatalf("%v\n", signErr)
-        }
+		// sign first tx with tweaked priv key and
+		// any remaining txs with topup key
+		var sig *btcec.Signature
+		var signErr error
+		if txIt == 0 {
+			priv := client.GetKeyFromHash(attestedHash).PrivKey
+			sig, signErr = priv.Sign(txPreImageHash.CloneBytes())
+		} else {
+			sig, signErr = client.WalletPrivTopup.PrivKey.Sign(txPreImageHash.CloneBytes())
+		}
+		if signErr != nil {
+			log.Fatalf("%v\n", signErr)
+		}
 
-        // add hash type to signature as well
-        sigBytes := append(sig.Serialize(), []byte{byte(1)}...)
+		// add hash type to signature as well
+		sigBytes := append(sig.Serialize(), []byte{byte(1)}...)
 
-        fmt.Printf("sending sig(%d) %s\n", txIt, hex.EncodeToString(sigBytes))
+		fmt.Printf("sending sig(%d) %s\n", txIt, hex.EncodeToString(sigBytes))
 
-        sigs = append(sigs, sigBytes)
-    }
+		sigs = append(sigs, sigBytes)
+	}
 
-    serializedSigs := attestation.SerializeBytes(sigs)
+	serializedSigs := attestation.SerializeBytes(sigs)
 	pub.SendMessage(serializedSigs, attestation.TOPIC_SIGS)
 }
