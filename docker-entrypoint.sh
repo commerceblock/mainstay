693ad3b2b2db47c84356e4294597c2654f77e60e
nkostoulas <nkostoulas@gmail.com>
2019-01-08 11:00:27 +0000

    Add entry point for dummy transaction signers

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-export API_HOST="$(uname -n):8080"
-
 if [[ "$1" == "mainstay" ]]; then
     echo "Running attestation"
-    mainstay -tx ${TX}
+    mainstay
+elif [[ "$1" == "signer1" ]]; then
+    echo "Running signer 1"
+    go run $GOPATH/src/mainstay/cmd/txsigningtool/txsigningtool.go -pk $PRIV_1 -pkTopup $PRIV_TOPUP_1 -host $HOST_1
+elif [[ "$1" == "signer2" ]]; then
+    echo "Running signer 2"
+    go run $GOPATH/src/mainstay/cmd/txsigningtool/txsigningtool.go -pk $PRIV_2 -pkTopup $PRIV_TOPUP_2 -host $HOST_2
 else
   $@
 fi
