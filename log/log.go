7e8f9683420b255523b8a984c2923107fae224f0
tomossomot <tomoswootton@protonmail.com>
2019-11-11 10:43:25 +0000

    comment log.go

--- log/log.go
+++ log/log.go
@@ -10,7 +10,8 @@ import (
     "fmt"
 )
 
-/* 	Extended logging functionality providing Info, Warn and Error messages for
+/*
+    Extended logging functionality providing Info, Warn and Error messages for
 	log entries.
 	Functions Infof(), Warnf(), Errorf() handles parameters in the same
 	way that fmt.Printf handles parameters.
@@ -22,35 +23,43 @@ var infoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile
 var warnLogger = log.New(os.Stdout, "WARN: ", log.Ldate|log.Ltime|log.Lshortfile)
 var errorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 
-
+// Info log entries print a message only, use for standard message output
+// standard INFO print
 func Info(v ...interface{}) {
     infoLogger.Output(2, fmt.Sprint(v...))
 }
+// INFO print with formatting
 func Infof(format string, v ...interface{}) {
     infoLogger.Output(2, fmt.Sprintf(format, v...))
 }
+// standard INFO print with new line
 func Infoln(v ...interface{}) {
     infoLogger.Output(2, fmt.Sprintln(v...))
 }
 
+// Warn log entries print a message only but wiht WARN marker, use for non-fatal
+// errors
+// standard WARN print
 func Warn(v ...interface{}) {
     warnLogger.Output(2, fmt.Sprint(v...))
 }
+// WARN print with formatting
 func Warnf(format string, v ...interface{}) {
     warnLogger.Output(2, fmt.Sprintf(format, v...))
 }
+// standard WARN print with new line
 func Warnln(v ...interface{}) {
     warnLogger.Output(2, fmt.Sprintln(v...))
 }
 
+// Error log entries print a message then halt execution
+// standard ERROR print.
 func Error(v ...interface{}) {
 	errorLogger.Output(2, fmt.Sprint(v...))
 	os.Exit(1)
 }
+// ERROR print with formatting
 func Errorf(format string, v ...interface{}) {
     errorLogger.Output(2, fmt.Sprintf(format, v...))
     os.Exit(1)
-}
-func Errorln(v ...interface{}) {
-    errorLogger.Output(2, fmt.Sprintln(v...))
 }
